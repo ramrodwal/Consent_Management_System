6e8467c8738a61591978c0a64b4854893b6b4ac5
RahulTejpal <rahul.tejpal@iiitb.ac.in>
2023-03-21 19:05:39 +0530

    front end page addition

---
+++ frontend/src/components/DoctorDashboardComponents/DoctorAppointments.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DoctorAppointments(){
+    return (
+        <>
+            <h1>Doctor Appointments</h1>
+            
+        </>
+  );
+
+}
+
+export default DoctorAppointments;
---
+++ frontend/src/components/DoctorDashboardComponents/DoctorProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DoctorProfile(){
+    return (
+        <>
+            <h1>Doctor Profile</h1>
+            
+        </>
+  );
+
+}
+
+export default DoctorProfile;
