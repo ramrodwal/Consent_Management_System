feecf0069112bb62e8210e5fb4ff3f497af97a04
gopal gupta <gopalgupta2804@gmail.com>
2023-03-21 20:05:41 +0530

    hospital app intialise

---
+++ Hospital/hospital_app/src/test/java/com/hospital/hospital_app/HospitalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.hospital_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
