a3726a3e366463650e66e085cd967d49440366dc
Vartika Chaturvedi <54345621+vartika1203@users.noreply.github.com>
2023-03-21 20:26:39 +0530

    initialised Patient backend

---
+++ Patient/patient_app/src/test/java/com/patient/patient_app/PatientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patient.patient_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
