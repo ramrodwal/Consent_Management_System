2b09103b0f9583fb2a39d81572256a5afc284312
Vartika Chaturvedi <54345621+vartika1203@users.noreply.github.com>
2023-03-22 20:22:23 +0530

    front pages

--- Patient/patient-frontend/src/App.js
+++ Patient/patient-frontend/src/App.js
@@ -1,23 +1,99 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Header from "./components/Header";
+import Homepage from './components/Homepage';
+import Login from './components/Login';
+import ForgotPassword from './components/ForgotPassword';
+import SignUp from './components/SignUp';
+// import './App.css';
+import PatientDashboard from './components/PatientDashboardComponents/PatientDashboard';
+import PatientNavbar from './components/PatientDashboardComponents/PatientNavbar';
+import Notification from './components/PatientDashboardComponents/Notification';
+import Profile from './components/PatientDashboardComponents/Profile';
+import Appointments from './components/PatientDashboardComponents/Appointments';
+// import VerifyOTP from './components/VerifyOTP';
+import { BrowserRouter as Router, Routes, Route, Redirect,} from "react-router-dom";
 
+function Home(){
+  return (
+    <>
+      <Header />
+      <Homepage />
+    </>
+  );
+}
+function LoginPage(){
+return (
+  <>
+    <Header />
+    <Login />
+  </>
+);
+}
+function ForgotPass(){
+return (
+  <>
+    <Header />
+    <ForgotPassword />
+  </>
+);
+}
+function SignUpPage(){
+  return (
+    <>
+      <Header />
+      <SignUp />
+    </>
+  );
+}
+function PatientDash(){
+  return(
+      <>
+      <PatientNavbar />
+      <PatientDashboard/>
+      </>
+  );
+}function Notifi(){
+  return(
+    <>
+    <PatientNavbar/>
+    <Notification/>
+    </>
+  );
+}
+function Prof(){
+  return(
+    <>
+    <PatientNavbar/>
+    <Profile/>
+    </>
+  );
+}
+function Appoin(){
+  return(
+    <>
+    <PatientNavbar/>
+    <Appointments/>
+    </>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Routes>
+      <Route exact path={"/"} element={<Home />}></Route> 
+        <Route exact path={"/Login.js"} element={<LoginPage />}></Route>
+        <Route exact path={"/ForgotPassword.js"} element={<ForgotPass />}></Route>
+        <Route exact path={"/SignUp.js"} element={<SignUpPage />}></Route>
+        <Route exact path={"/PatientDashboard.js"} element={<PatientDash/>}></Route>
+        <Route exact path={"/Notification.js"} element={<Notifi />}></Route>
+        <Route exact path={"/Profile.js"} element={<Prof />}></Route>
+        <Route exact path={"/Appointments.js"} element={<Appoin/>}></Route>
+        {/* <Route exact path={"/VerifyOTP.js"} element={<VerifyOTP/>}></Route> */}
+        {/* <Route exact path={"/PatientDetails.js"} element={<PatientDet />}></Route> */}
+      </Routes>
+      </Router>
     </div>
   );
 }
