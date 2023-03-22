6babb01d467ca1d3dfc63fee987cb83d81b90572
ramrodwal <rrodwal22@gmail.com>
2023-03-22 20:24:01 +0530

    added controllers

---
+++ Patient/patient_app/src/main/java/com/patient/patient_app/web/ConsentController.java
@@ -0,0 +1,10 @@
+package com.patient.patient_app.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsentController {
+    
+    
+
+}
---
+++ Patient/patient_app/src/main/java/com/patient/patient_app/web/PatientController.java
@@ -0,0 +1,14 @@
+package com.patient.patient_app.web;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.patient.patient_app.entity.Patient;
+
+@RestController
+public class PatientController {
+
+    
+}
