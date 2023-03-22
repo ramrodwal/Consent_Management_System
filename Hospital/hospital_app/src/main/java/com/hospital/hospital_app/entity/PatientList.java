c83de754fccccf9a36aeca9868ced34a3c54a4a7
gopal gupta <gopalgupta2804@gmail.com>
2023-03-22 18:33:48 +0530

    added pojo classes to hospital side

---
+++ Hospital/hospital_app/src/main/java/com/hospital/hospital_app/entity/PatientList.java
@@ -0,0 +1,18 @@
+package com.hospital.hospital_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class PatientList {
+    
+        private int hospital_id;
+        private String patient_aadhar;
+
+}
