a2f1edb6d8f5202ec19c20fbea3b52b1c381ed0e
gopal gupta <gopalgupta2804@gmail.com>
2023-03-22 19:32:22 +0530

    admin auth

---
+++ Hospital/hospital_app/src/main/java/com/hospital/hospital_app/entity/Admin.java
@@ -0,0 +1,17 @@
+package com.hospital.hospital_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Admin {
+    private String username;
+    private String password;
+
+}
---
+++ Hospital/hospital_app/src/main/java/com/hospital/hospital_app/web/RecordController.java
@@ -0,0 +1,8 @@
+package com.hospital.hospital_app.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecordController {
+    
+}
