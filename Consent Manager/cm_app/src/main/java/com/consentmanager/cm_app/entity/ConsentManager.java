f68094675a1ba7740e1ae67cdeba30dd9ef63b20
Vartika Chaturvedi <54345621+vartika1203@users.noreply.github.com>
2023-03-23 11:42:40 +0530

    changes

--- Consent Manager/cm_app/src/main/java/com/consentmanager/cm_app/entity/ConsentManager.java
+++ Consent Manager/cm_app/src/main/java/com/consentmanager/cm_app/entity/ConsentManager.java
@@ -1,22 +1,20 @@
 package com.consentmanager.cm_app.entity;
 
 
-import org.springframework.data.relational.core.mapping.Table;
+
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+
 import lombok.Setter;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name= "consentmanager")
-@Data
+
 
 
 public class ConsentManager {
