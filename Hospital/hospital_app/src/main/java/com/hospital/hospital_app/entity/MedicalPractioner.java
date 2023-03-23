815faa863904cd81aa5e6aa5c10ecdf8f7cfd27a
gopal gupta <gopalgupta2804@gmail.com>
2023-03-23 10:51:28 +0530

    hospital side table creation

--- Hospital/hospital_app/src/main/java/com/hospital/hospital_app/entity/MedicalPractioner.java
+++ Hospital/hospital_app/src/main/java/com/hospital/hospital_app/entity/MedicalPractioner.java
@@ -1,5 +1,14 @@
 package com.hospital.hospital_app.entity;
 
+import org.springframework.format.annotation.NumberFormat;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,25 +18,105 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "MedicalPractitioner")
 public class MedicalPractioner {
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "first name can not be empty")
     private String fname;
+
+    @Column()
     private String mname;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "last name can not be empty")
     private String lname;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "age can not be empty")
     private int age;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "gender can not be empty")
     private String gender;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "email can not be empty")
+    @Email(message = "enter a valid email address")
     private String email;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "number can not be empty")
+    @NumberFormat
     private String number;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "state can not be empty")
     private String state;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "city can not be empty")
     private String city;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "address can not be empty")
     private String address;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "zipcode can not be empty")
     private int zipcode;
+
+    @Id
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "aadhar number can not be empty")
     private String practioner_aadhar;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "medical license id can not be empty")
     private String medical_license_id;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "specialisation can not be empty")
     private String specialisation;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "qualification can not be empty")
     private String qualification;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "username can not be empty")
     private String username;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "password can not be empty")
     private String password;
+
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "confirm password can not be empty")
     private String confirmPassword;
+
+    @Id
+    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "hospital id can not be empty")
     private int hospital_id;
 
 
--- Hospital/hospital_app/src/main/java/com/hospital/hospital_app/entity/PatientList.java
+++ Hospital/hospital_app/src/main/java/com/hospital/hospital_app/entity/PatientList.java
@@ -1,5 +1,11 @@
 package com.hospital.hospital_app.entity;
 
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +15,19 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "PatientList")
 
 public class PatientList {
-    
+        @Id
+        @Column(nullable = false)
+        @NonNull
+        @NotBlank(message = "hospital id can not be empty")
         private int hospital_id;
+
+        @Column(nullable = false)
+        @NonNull
+        @NotBlank(message = "patient aadhar number can not be empty")
         private String patient_aadhar;
 
 }
