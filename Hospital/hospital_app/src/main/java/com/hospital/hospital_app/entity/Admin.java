c01b8701e264fca5824810242a9a51d1457c1a6e
gopal gupta <gopalgupta2804@gmail.com>
2023-03-23 10:13:44 +0530

    trail commit

--- Hospital/hospital_app/src/main/java/com/hospital/hospital_app/entity/Admin.java
+++ Hospital/hospital_app/src/main/java/com/hospital/hospital_app/entity/Admin.java
@@ -1,17 +1,22 @@
 package com.hospital.hospital_app.entity;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Admin {
+
     private String username;
+
     private String password;
 
 }
