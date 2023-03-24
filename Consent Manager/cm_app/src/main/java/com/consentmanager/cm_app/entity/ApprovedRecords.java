63342d79e556f6571ad381c5d3d6ca0aab46ca73
gopal gupta <gopalgupta2804@gmail.com>
2023-03-24 12:24:02 +0530

    hospital side three layer architecture

--- Consent Manager/cm_app/src/main/java/com/consentmanager/cm_app/entity/ApprovedRecords.java
+++ Consent Manager/cm_app/src/main/java/com/consentmanager/cm_app/entity/ApprovedRecords.java
@@ -10,7 +10,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
