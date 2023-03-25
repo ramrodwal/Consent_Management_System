5059429ca89a118e19a787d6155db0abfada772f
RahulTejpal <rahul.tejpal@iiitb.ac.in>
2023-03-25 15:19:30 +0530

    Doctor Login-frontend

--- Hospital/hospital-frontend/src/DoctorLogin.js
+++ Hospital/hospital-frontend/src/DoctorLogin.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function DoctorLogin(){
     return(
         <>
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      
+      <Navbar.Brand href="/HospitalHome">Hospital Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/BookAppointmentDoctor">Book Appointment</Nav.Link>
+          <Nav.Link href="/DoctorLogin">Doctors' Login</Nav.Link>
+          <Nav.Link href="/AdminLogin">Admin Login</Nav.Link>
+          
+          
+        </Nav>
+        
+      </Navbar.Collapse>
+    
+  </Navbar>
+
+            <div className='pageheading'>
             <center><h1>Doctor's Login</h1></center>
+            </div>
 
             <div className="Auth-form-container">
       <form className="Auth-form">
         <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
+          <h3 className="Auth-form-title">Login</h3>
           <div className="form-group mt-3">
-            <label>Email address</label>
+            <label>Username</label>
             <input
-              type="email"
+              type="text"
               className="form-control mt-1"
-              placeholder="Enter email"
+              placeholder="Enter Username"
             />
           </div>
           <div className="form-group mt-3">
@@ -30,9 +52,7 @@ function DoctorLogin(){
               Submit
             </button>
           </div>
-          <p className="forgot-password text-right mt-2">
-            Forgot <a href="#">password?</a>
-          </p>
+          
         </div>
       </form>
     </div>
