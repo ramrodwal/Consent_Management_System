e3dad85b475f340c6f0147b3b84b4384fe89f734
RahulTejpal <rahul.tejpal@iiitb.ac.in>
2023-03-25 14:56:11 +0530

    Hospital Home-frontend

--- Hospital/hospital-frontend/src/HospitalHome.js
+++ Hospital/hospital-frontend/src/HospitalHome.js
@@ -1,30 +1,66 @@
 import React from "react";
+import Container from 'react-bootstrap/esm/Container';
+import "./HospitalComponents/HospitalStyle.css"
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
-import Card from "react-bootstrap/Card";
+
 
 
 function HospitalHome(){
     return(
         <>
-        
-            <h1>Hospital Home</h1>
-            <container>
-                <Card>
-                <a class="logindoc" href="/BookAppointmentDoctor">
-                    Book Appointment
-                    </a>
-                </Card>
-                <Card>
-                <a class="logindoc" href="/DoctorLogin">
-                    Doctor's Login
-                    </a>
-                </Card>
-                <Card>
-                <a class="logindoc" href="/AdminLogin">
-                    Admin Login
-                    </a>
-                </Card>
-            </container>
+        <div className="pageheading">
+        <center><h1>Hospital Home</h1></center><br></br>
+        </div>
+        <Container>
+            
+           
+
+<CardGroup  className='topgroup'>
+  <Card>
+    <a href='/BookAppointmentDoctor'>
+    <Card.Img variant="top" className='Images' src="https://media.istockphoto.com/id/1286487110/vector/appointment-booking-calendar-vector-illustration.jpg?s=612x612&w=0&k=20&c=K26phQ_nb0tu7Ezx9Fvf_43_5J-bOkufOUf_HfqNlXA=" />
+    </a>
+    <Card.Body>
+      
+      
+    </Card.Body>
+    <Card.Footer>
+    <h3><b>Book an appointment</b></h3>
+    </Card.Footer> 
+    
+  </Card>
+  <Card>
+    <a href='/DoctorLogin'>
+    <Card.Img variant="top" className='Images' src="https://img.freepik.com/premium-vector/doctor-icon-avatar-white_136162-58.jpg?w=2000" />
+    </a>
+    <Card.Body>
+      
+      
+    </Card.Body>
+    <Card.Footer>
+      <h3><b>Doctor's Login</b></h3>
+    </Card.Footer>
+   
+  </Card>
+
+  <Card>
+    <a href='/AdminLogin'>
+    <Card.Img variant="top" className='Images' src="https://static.vecteezy.com/system/resources/previews/003/190/658/original/line-icon-for-back-office-vector.jpg" />
+    </a>
+    <Card.Body>
+      
+      
+    </Card.Body>
+    <Card.Footer>
+      <h3><b>Admin Login</b></h3>
+    </Card.Footer>
+   
+  </Card>
+  
+</CardGroup>
+            </Container>
 
         </>
 
