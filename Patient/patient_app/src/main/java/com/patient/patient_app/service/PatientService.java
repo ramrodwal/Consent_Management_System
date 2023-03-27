7445b6700fd1855ab7850149233785b77e120452
ramrodwal <rrodwal22@gmail.com>
2023-03-27 18:54:12 +0530

    getPatientByid or view profile of patient Id api working properly in frontend as well as postman

--- Patient/patient_app/src/main/java/com/patient/patient_app/service/PatientService.java
+++ Patient/patient_app/src/main/java/com/patient/patient_app/service/PatientService.java
@@ -5,5 +5,6 @@ import com.patient.patient_app.entity.Patient;
 public interface PatientService {
 
     Patient registerPatient(Patient patient);
+    Patient getPatient(String patient_aadhar);
     
 }
