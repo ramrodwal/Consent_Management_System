cacf4d14103c624fa49930b8aef8e292f67ed00a
Vartika Chaturvedi <54345621+vartika1203@users.noreply.github.com>
2023-03-29 22:36:49 +0530

    improved api for communication between patient and hospital

--- Patient/patient_app/src/main/java/com/patient/patient_app/service/RecordServiceImpl.java
+++ Patient/patient_app/src/main/java/com/patient/patient_app/service/RecordServiceImpl.java
@@ -16,19 +16,24 @@ public class RecordServiceImpl implements RecordService{
 
     private RecordMetaDataRepository recordMetaDataRepository;
 
-    @Override
-    public RecordMapping addData(MedicalRecords medicalRecords) {
+    // @Override
+    // public RecordMapping addData(MedicalRecords medicalRecords) {
         
-        int hospital_id = medicalRecords.getHospital_id();
-        String patient_aadhar = medicalRecords.getPatient_aadhar();
-        int Id=1;
-        Patient patient=new Patient(null, "ram", "", "rodwal", 23, "male", "ram@gmail.com", "1234567896", "hello", "hello", "123456", "afdsf", "123456789456", "dfsdf", "fsdffsd", "fsdfs");
-        patient.setPatient_aadhar(patient_aadhar);
+    //     int hospital_id = medicalRecords.getHospital_id();
+    //     String patient_aadhar = medicalRecords.getPatient_aadhar();
+    //     int Id=1;
+    //     Patient patient=new Patient(null, "ram", "", "rodwal", 23, "male", "ram@gmail.com", "1234567896", "hello", "hello", "123456", "afdsf", "123456789456", "dfsdf", "fsdffsd", "fsdfs");
+    //     patient.setPatient_aadhar(patient_aadhar);
 
-        RecordMapping recordMapping = new RecordMapping(Id++, hospital_id, patient);
+    //     RecordMapping recordMapping = new RecordMapping(Id++, hospital_id, patient);
 
+    //     return recordMetaDataRepository.save(recordMapping);
+
+    // }
+
+    @Override
+    public RecordMapping addData(RecordMapping recordMapping){
         return recordMetaDataRepository.save(recordMapping);
-
     }
 
     
