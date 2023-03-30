2df6f623c2a0551f6ebaa11d0cb675b227ccd877
Vartika Chaturvedi <54345621+vartika1203@users.noreply.github.com>
2023-03-30 15:42:34 +0530

    initialised book appointment api

--- Hospital/hospital_app/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO admin_details (username,password) VALUES ('gopalgupta@gmail.com','gopal121');
-INSERT INTO admin_details (username,password) VALUES ('ramrodwal@gmail.com','ram121');
-INSERT INTO admin_details (username,password) VALUES ('vartikachaturvedi@gmail.com','vartika121');
-INSERT INTO admin_details (username,password) VALUES ('rahultejpal@gmail.com','rahul121');
-
