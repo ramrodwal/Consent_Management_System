52b9a91980dc34d6da2b0b7238d4c8390f4ac807
gopal gupta <gopalgupta2804@gmail.com>
2023-03-30 15:55:16 +0530

    data.sql file at hospital side

---
+++ Hospital/hospital_app/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into admin_details(username,password) values("gopalgupta@gmail.com","gopal123");
+insert into admin_details(username,password) values("vartikachutiya@gmail.com","vartika123");
+insert into admin_details(username,password) values("ramrodwal@gmail.com","ram123");
+insert into admin_details(username,password) values("rahultejpal@gmail.com","rahul123");
