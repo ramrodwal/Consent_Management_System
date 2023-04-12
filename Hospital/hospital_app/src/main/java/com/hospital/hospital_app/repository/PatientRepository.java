657c3fe861b7aeac330b620f695aa6b0e4fc6514
ramrodwal <rrodwal22@gmail.com>
2023-04-12 17:33:38 +0530

    Patient backend updated

--- Hospital/hospital_app/src/main/java/com/hospital/hospital_app/repository/PatientRepository.java
+++ Hospital/hospital_app/src/main/java/com/hospital/hospital_app/repository/PatientRepository.java
@@ -3,7 +3,11 @@ package com.hospital.hospital_app.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.hospital.hospital_app.entity.PatientList;
+import java.util.List;
+
 
 public interface PatientRepository extends CrudRepository<PatientList, String>{
     
+     List<PatientList> findByMedicalPractitioner_practitionerAadhar(String practitioner_aadhar);
+
 }
