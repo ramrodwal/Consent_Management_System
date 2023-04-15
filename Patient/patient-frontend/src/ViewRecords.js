3f4312cacf3cb3283213dd891e273df440ee5299
ramrodwal <rrodwal22@gmail.com>
2023-04-15 11:34:06 +0530

    updated viewrecords frontene

--- Patient/patient-frontend/src/ViewRecords.js
+++ Patient/patient-frontend/src/ViewRecords.js
@@ -7,75 +7,80 @@ import axios from 'axios';
 
 
 
-function ViewRecords(){
+function ViewRecords() {
 
-  const [records,setRecords]=useState([]);
+  const [records, setRecords] = useState([]);
 
-  let patientAadhar="666666666666";
 
   useEffect(() => {
-    axios.get(`http://localhost:9099/hospital/record-mapping/${patientAadhar}`).then((response) => {
-      console.log(response);
-      setRecords(response.data);
-      console.log(records)
-    });
+
+    const fetchRecord = async () => {
+      const token = localStorage.getItem('authToken'); // get token from localStorage
+      const id = localStorage.getItem('id');
+      const headers = { Authorization: `Bearer ${token}` }; // add token to headers
+      const { data } = await axios.get(`http://localhost:9099/hospital/record-mapping/${id}`, { headers });
+      console.log(data);
+      setRecords(data); 
+    };
+  
+    fetchRecord();
   }, []);
 
 
-  
-
-    return(
-        <>
-
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      
-      <Navbar.Brand href="/PatientDashboard.js">Patient</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="/AllRequest.js">View All Requests</Nav.Link>
-          <Nav.Link href="/PatientProfileView.js">View Profile</Nav.Link>
-          <Nav.Link href="/ViewRecords.js">View Records</Nav.Link>
-          
-          
-        </Nav>
-        <Nav>
-          <Nav.Link href="/">Logout</Nav.Link>
-         
-        </Nav>
-      </Navbar.Collapse>
-    
-  </Navbar>
-            <center><h1 className='pageheading'>View Records</h1></center>
 
 
-            <Table stripped bordered hover variant="dark" size="sm">
-  <thead>
-    <tr className='tablehead'>
-      <th className='tabledata'>Record Id</th>
-      <th className='tabledata'>Disease Name</th>  
-      <th className='tabledata'>Patient Aadhar</th>
-      <th className='tabledata'>Priescription</th>
-      <th className='tabledata'>Hospital Id</th>
-      <th className='tabledata'>Practitioners Aadhar</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-  {records.map((record) => (
-    <tr key={record.id}>
-        <td className='tabledata'>{record.record_id}</td>
-        <td className='tabledata'>{record.disease_name}</td>
-        <td className='tabledata'>{record.patientAadhar}</td>
-        <td className='tabledata'>{record.record}</td>
-        <td className='tabledata'>{record.centralHospital.hospitalId}</td>
-        <td className='tabledata'>{record.medicalPractitioner.practitionerAadhar}</td>
-    </tr>
-  ))}
-  </tbody>
-</Table>
-        </>
-    )
+  return (
+    <>
+
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+        <Navbar.Brand href="/PatientDashboard.js">Patient</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/AllRequest.js">View All Requests</Nav.Link>
+            <Nav.Link href="/PatientProfileView.js">View Profile</Nav.Link>
+            <Nav.Link href="/ViewRecords.js">View Records</Nav.Link>
+
+
+          </Nav>
+          <Nav>
+            <Nav.Link href="/">Logout</Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar>
+      <center><h1 className='pageheading'>View Records</h1></center>
+
+
+      <Table stripped bordered hover variant="dark" size="sm">
+        <thead>
+          <tr className='tablehead'>
+            <th className='tabledata'>Record Id</th>
+            <th className='tabledata'>Disease Name</th>
+            <th className='tabledata'>Patient Aadhar</th>
+            <th className='tabledata'>Priescription</th>
+            <th className='tabledata'>Hospital Id</th>
+            <th className='tabledata'>Practitioners Aadhar</th>
+
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((record) => (
+            <tr key={record.id}>
+              <td className='tabledata'>{record.record_id}</td>
+              <td className='tabledata'>{record.disease_name}</td>
+              <td className='tabledata'>{record.patientAadhar}</td>
+              <td className='tabledata'>{record.record}</td>
+              <td className='tabledata'>{record.centralHospital.hospitalId}</td>
+              <td className='tabledata'>{record.medicalPractitioner.practitionerAadhar}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  )
 }
 
 export default ViewRecords;
